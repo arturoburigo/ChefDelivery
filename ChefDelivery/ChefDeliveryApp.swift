f2ffcb5b87b0eff0779b413bff088db64e652e8b
Arturo Burigo <burigoarturo3@gmail.com>
2024-07-08 19:43:04 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Arturo Burigo on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
